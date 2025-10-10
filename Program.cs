93b3e466c37f408ee91e9f2d09678587cfa2f532
Gabriel Sanz <gabriel.sanz@infortechms.com.br>
2025-10-10 09:40:38 -0400

    fix: Corrigir RoutePrefix do Swagger para funcionar em /swagger

--- Program.cs
+++ Program.cs
@@ -186,7 +186,7 @@ app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "FormEngine API v1");
-    c.RoutePrefix = string.Empty; // Swagger na raiz
+    c.RoutePrefix = "swagger"; // Swagger em /swagger
 });
 
 app.UseHttpsRedirection();
